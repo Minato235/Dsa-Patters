3e0199f459c8d3748131be36be213c61202cb972
Minato <ankamasish@gmail.com>
2022-12-27 13:20:43 +0530

    naive string remaing

---
+++ 5. Searching Algorithms/binary.js
@@ -0,0 +1,17 @@
+function Binarysearch(array,val){
+    let min=0,max=array.length-1;
+ while(min<=max){
+    let mid=Math.floor((max+min)/2);
+    // let current=array[mid];
+    if(array[mid]>val){
+        max=mid-1;
+    }else if(array[mid]<val){
+        min=mid+1;
+    }else{
+        return mid;
+    }
+ }
+ return -1;
+    }
+//O(log n)
+console.log(Binarysearch([1,2,3,4,5],2));
