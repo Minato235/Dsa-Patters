c236b47bf15548fae066f4654ded3d6a550852fc
Minato <ankamasish@gmail.com>
2023-01-06 11:07:01 +0530

    ...

---
+++ 0.Maps,filter etc/destructuring .js
@@ -0,0 +1,6 @@
+//The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+let a,b,rest;
+[a,b]=[10,20];
+console.log(a,b);
+[a, b,] = [10, 20, 30, 40, 50];
+console.log(rest)
---
+++ 0.Maps,filter etc/forEach.js
@@ -0,0 +1,18 @@
+//forEach
+const array1 = ['a', 'b', 'c'];
+array1.forEach(ele=>{
+    // console.log(ele)
+})
+
+var arr = [3, 5, 7];
+arr.foo = "hello";
+    
+for (var i in arr) {
+  console.log(i); // logs "0", "1", "2", "foo"
+}
+console.log("======of=====")
+    
+for (var i of arr) {
+  console.log(i); // logs "3", "5", "7"
+  // it doesn't log "3", "5", "7", "hello"
+}
