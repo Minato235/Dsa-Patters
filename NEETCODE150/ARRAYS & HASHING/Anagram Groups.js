50b1442e5d7e3cd1804d86ac788aae9306db5fb4
Minato235 <ankamasish@gmail.com>
2024-06-19 10:14:35 -0400

    only 3 done we will complete this week

---
+++ NEETCODE150/ARRAYS & HASHING/Anagram Groups.js
@@ -0,0 +1,10 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {}
+}
+let solution = new Solution();
+
+let strs = ["act","pots","tops","cat","stop","hat"];
---
+++ NEETCODE150/ARRAYS & HASHING/Two Integer Sum.js
@@ -0,0 +1,29 @@
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let map = new Map();
+        for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+            const comp=target-num;
+            console.log(comp)
+            if(map.has(comp)){
+                return [map.get(comp),i]
+            }
+             map.set(num,i)
+        }
+        return[-1,-1]
+    }
+}
+
+
+let solution = new Solution();
+
+let nums = [3, 4, 5, 6], target = 7;
+
+
+console.log(solution.twoSum(nums, target));
