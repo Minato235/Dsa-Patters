702df4c815cbabc2b078ff32b5e04582d6d79561
Minato235 <ankamasish@gmail.com>
2024-10-28 21:46:11 -0400

    day 4 done

--- JAVA NOTEs/One.java
+++ JAVA NOTEs/One.java
@@ -1,5 +1,15 @@
 class One{
     public static void main(String[] args) {
-        
+        System.out.println("hello");
+        One one =new One();
+        one.drill();
+        int goals=one.pass(10);
+        System.out.println(goals);
+    }
+    public void drill(){
+        System.out.println("do training");
+    }
+    public int pass(int goals){
+        return goals*5;
     }
 }
